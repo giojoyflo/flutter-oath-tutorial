71eeeabe7dacc8aad1ae7eaa5f0def7a822eb03b
giorgia paparazzo <giorgia.paparazzo@joyflo.com>
2023-02-27 09:39:12 +0100

    add git connection

---
+++ oauth_tutorial/lib/github_oauth_credentials.dart
@@ -0,0 +1,6 @@
+// TODO: Create an OAuth App
+const githubClientId = '40a3dae49b4b7e56fb9b';
+const githubClientSecret = '7ff8aec025feca34ffaff63e2c0d9c007bbbebe7';
+
+// OAuth scopes for repository and user information
+const githubScopes = ['repo', 'read:org'];
